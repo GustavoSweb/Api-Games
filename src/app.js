c68a4aa749d912a3ab65bee843917a258ffd7684
Gustavo-Rodrigues <gustavodasilvama10@gmail.com>
2023-12-17 21:43:50 -0300

    Game routes created, game deletion, and game creation done

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser")
 
 const database = require("./config/database/database")
 const Games = require("./models/Games")
-
+const gamesRoutes = require("./router/Games")
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+app.use("/", gamesRoutes)
 
 async function main(){
     try{
@@ -18,34 +20,10 @@ async function main(){
         console.error(err)
     }
 }
+
 main()
 
-app.get("/games", async (req, res)=>{
-    try{
-        const games = await Games.findAll()
-        res.statusCode = 200
-        res.json(games)
-    }catch(err){
-        res.sendStatus(500)
-    }
-})
-
-app.get("/game/:id", async (req, res)=>{
-    const {id} = req.params
-
-    if (isNaN(id)) return res.sendStatus(400)
-
-    try{
-        const game = await Games.findByPk(id)
-        if(!game) return res.sendStatus(404)
-        res.json(game)
-    }catch(err){
-        res.sendStatus(500)
-    }
-
-
-
-})
-app.listen(8081, ()=>{
+const PORT = process.env.PORT || 8081
+app.listen(PORT, ()=>{
     console.log("Servidor Rodando")
 })
---
+++ src/router/Games.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const Router = express.Router();
+const Games = require("../models/Games");
+
+Router.get("/games", async (req, res) => {
+  try {
+    const games = await Games.findAll();
+    res.statusCode = 200;
+    res.json(games);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+Router.get("/game/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (isNaN(id)) return res.sendStatus(400);
+
+  try {
+    const game = await Games.findByPk(id);
+    if (!game) return res.sendStatus(404);
+    res.json(game);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+Router.post("/game", async (req, res) => {
+  const { title, year, price } = req.body;
+
+  if (!title || !year || !price) return res.sendStatus(400);
+  if (isNaN(year) || isNaN(price)) return res.sendStatus(400);
+  try {
+    await Games.create(req.body);
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+Router.delete("/game/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (isNaN(id)) return res.sendStatus(400);
+
+  try {
+    const game = await Games.findByPk(id);
+    if (!game) return res.sendStatus(404);
+    await game.destroy();
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+module.exports = Router;
