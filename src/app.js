66f34b3b658c69c1c0d98a5d163aea2ac9f006da
Gustavo-Rodrigues <gustavodasilvama10@gmail.com>
2023-12-17 21:16:28 -0300

    Created end point games and game/id

--- src/app.js
+++ src/app.js
@@ -18,9 +18,34 @@ async function main(){
         console.error(err)
     }
 }
-
 main()
 
+app.get("/games", async (req, res)=>{
+    try{
+        const games = await Games.findAll()
+        res.statusCode = 200
+        res.json(games)
+    }catch(err){
+        res.sendStatus(500)
+    }
+})
+
+app.get("/game/:id", async (req, res)=>{
+    const {id} = req.params
+
+    if (isNaN(id)) return res.sendStatus(400)
+
+    try{
+        const game = await Games.findByPk(id)
+        if(!game) return res.sendStatus(404)
+        res.json(game)
+    }catch(err){
+        res.sendStatus(500)
+    }
+
+
+
+})
 app.listen(8081, ()=>{
     console.log("Servidor Rodando")
 })
